0839fba1dd6822eaec1cd46b277023dc1b2e4bc2
birdy90 <gob@live.ru>
2015-05-02 13:04:35 +0300

    Added project

---
+++ KnapsackForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Knapsack
+{
+    partial class KnapsackForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.solve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 12);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(162, 162);
+            this.input.TabIndex = 1;
+            this.input.Text = "14\r\n8 4\r\n10 5\r\n15 8\r\n4 3";
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(180, 12);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(464, 211);
+            this.output.TabIndex = 2;
+            // 
+            // solve
+            // 
+            this.solve.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.solve.Location = new System.Drawing.Point(12, 180);
+            this.solve.Name = "solve";
+            this.solve.Size = new System.Drawing.Size(162, 43);
+            this.solve.TabIndex = 0;
+            this.solve.Text = "Решить";
+            this.solve.UseVisualStyleBackColor = true;
+            this.solve.Click += new System.EventHandler(this.solve_Click);
+            // 
+            // KnapsackForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 235);
+            this.Controls.Add(this.solve);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Name = "KnapsackForm";
+            this.Text = "Упаковка рюкзака";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button solve;
+    }
+}
+
---
+++ KnapsackForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Knapsack
+{
+    public partial class KnapsackForm : Form
+    {
+        int[] _values;
+        int[] _weights;
+        int[] _appliance;
+
+        public KnapsackForm()
+        {
+            InitializeComponent();
+        }
+
+        int knapsack(int[] weights, int[] values, int capacity, int numberOfItems, out int[,][] usages)
+        {
+            var fullNumber = weights.Length;
+            int[,] dp = new int[capacity + 1, fullNumber + 1];
+            usages = new int[capacity + 1, fullNumber + 1][];
+            // дополнительно формируем массивы для запоминания того, какие предметы сколько раз были добавлены в рюкзак
+            for (int i = 0; i <= fullNumber; i++)
+            {
+                for (int j = 0; j <= capacity; j++)
+                {
+                    usages[j, i] = new int[numberOfItems];
+                }
+            }
+
+            for (int i = 1; i < fullNumber + 1; i++) // для каждого возможного использования количества предметов
+            {
+                for (int w = 1; w < capacity + 1; w++) // для каждой возрастающей емкости рюкзака
+                {
+                    // копируем индексы предметов, которые были добавлены для получения текущей ценности
+                    for (var k = 0; k < numberOfItems; k++)
+                        usages[w, i][k] = usages[w, i - 1][k];
+                    if (weights[i - 1] <= w) // если новый предмет в принципе впихивается в рюкзак
+                    {
+                        // то пытаемся впихнуть его, или оставить предыдущее решение
+                        dp[w, i] = Math.Max(dp[w, i - 1], dp[w - weights[i - 1], i - 1] + values[i - 1]);
+                        // если мы впихнули новый элемент, то увеличиваем индекс добавленного предмета
+                        if (dp[w, i - 1] < dp[w - weights[i - 1], i - 1] + values[i - 1])
+                        {
+                            for (var k = 0; k < numberOfItems; k++)
+                                usages[w, i][k] = usages[w - weights[i - 1], i - 1][k];
+                            usages[w, i][_appliance[i - 1]]++;
+                        }
+                    }
+                    else
+                    {
+                        // оставляем предыдущее решение
+                        dp[w, i] = dp[w, i - 1];
+                    }
+                }
+            }
+
+            return dp[capacity, fullNumber];
+        }
+
+        private void solve_Click(object sender, EventArgs e)
+        {
+            int number;
+            int fullNumber;
+            int capacity;
+            int[,][] usages;
+
+            output.Clear();
+
+            var inputDataRows = input.Text.Split("\n".ToCharArray());
+            capacity = Convert.ToInt32(inputDataRows[0]);
+
+            number = inputDataRows.Length - 1;
+            fullNumber = 0;
+            for (int i = 1; i < inputDataRows.Length; i++)
+                fullNumber += (int)Math.Floor(capacity / Convert.ToDouble(inputDataRows[i].Split(' ')[1]));
+
+            _values = new int[fullNumber];
+            _weights = new int[fullNumber];
+            _appliance = new int[fullNumber];
+
+            var k = 0;
+            for (int i = 1; i < inputDataRows.Length; i++)
+            {
+                var row = inputDataRows[i].Split(' ');
+                var thisNum = (int)Math.Floor(capacity / Convert.ToDouble(row[1]));
+                for (int j = 0; j < thisNum; j++)
+                {
+                    _values[k] = Convert.ToInt32(row[0]);
+                    _weights[k] = Convert.ToInt32(row[1]);
+                    _appliance[k] = i - 1;
+                    k++;
+                }
+            }
+            var result = knapsack(_weights, _values, capacity, number, out usages);
+
+            output.Text += result.ToString() + "\r\n";
+            for (int i = 0; i < number; i++)
+            {
+                if (usages[capacity, fullNumber][i] == 0) continue;
+                output.Text += string.Format("Предмет №{0} кладем {1} раз(а)\r\n", i + 1, usages[capacity, fullNumber][i]);
+            }
+        }
+    }
+}
